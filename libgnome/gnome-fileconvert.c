987f2557020fc4d85e8a25c39020bc3f802ed930
Elliot Lee <sopwith@src.gnome.org>
1998-01-02 03:12:10 +0000

    The file conversion code now can find indirect paths. It does not find the shortest path, just the first one. Because of this, it also does not find the best-quality path (by allowing quality ratings for filters). Maybe I'm stupid, but it took me long enough to figure the algorithm out for this - if you know of a way to do shortest-path and quality ratings, be my guest ;-)

--- libgnome/gnome-fileconvert.c
+++ libgnome/gnome-fileconvert.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <glib.h>
 
 #include "gnome-defs.h"
@@ -9,11 +10,23 @@
 
 #define TYPES_CVT_FILENAME "types.cvt"
 
+typedef struct _FileConverter FileConverter;
+struct _FileConverter {
+  gchar *fromtype;
+  gchar *totype;
+  gchar *cmdline;
+  gint tag; /* Used for knowing whether we have visited
+	       this node yet */
+  GList *links;
+};
+
+static GList *FileConverters = NULL; /* A list of all the FileConverter's we
+			       read in */
+
 static GList *
 gfc_get_path(gchar *fromtype, gchar *totype);
 static gint
 gfc_run_pipe(gchar *acmd, gint infd);
-static GList *pids;
 
 gint
 gnome_file_convert(gchar *filename, gchar *fromtype, gchar *totype)
@@ -30,26 +43,35 @@ gnome_file_convert_fd(gint fd, gchar *fromtype, gchar *totype)
 {
   GList *convlist, *l;
   gint infd, outfd;
+  FileConverter *anode;
   gchar *ccmd;
+
+  srand(time(NULL));
 
   convlist = gfc_get_path(fromtype, totype);
 
   for(l = convlist, infd = fd; l; l = l->next) {
-    ccmd = l->data;
-    outfd = gfc_run_pipe(ccmd, infd);
+    anode = l->data;
+    g_print("%s %s: %s\n", anode->fromtype, anode->totype,
+	    anode->cmdline);
+    if(anode->cmdline == NULL)
+      continue;
+    outfd = gfc_run_pipe(anode->cmdline, infd);
     if(infd != fd) close(infd);
     infd = outfd;
   }
   return infd;
 }
 
-static GList *
-gfc_get_path(gchar *fromtype, gchar *totype)
+/* Probably could use gnome_config routines for this,
+   as soon as miguel redoes it */
+static void
+gfc_read_FileConverters(void)
 {
-  GList *retval = NULL;
   gchar aline[512];
-  gchar **parts, *cmd;
+  gchar **parts;
   FILE *conffile;
+  FileConverter *newnode;
 
   conffile = fopen(TYPES_CVT_FILENAME, "r");
 
@@ -60,16 +82,88 @@ gfc_get_path(gchar *fromtype, gchar *totype)
 	 || aline[0] == '\0')
 	continue;
       parts = gnome_string_split(aline, " ", 3);
-
-      if(!strcmp(fromtype, parts[0])
-	 && !strcmp(totype, parts[1])) {
-	cmd = parts[2];
-	g_free(parts[0]); g_free(parts[1]); g_free(parts);
-	return g_list_append(retval, cmd);
-      }
+      newnode = g_new(FileConverter, 1);
+      newnode->fromtype = parts[0];
+      newnode->totype = parts[1];
+      newnode->cmdline = parts[2];
+      g_free(parts);
+      newnode->tag = 0; newnode->links = NULL;
+      FileConverters = g_list_append(FileConverters, newnode);
     }
   }
-  return retval;
+}
+
+static void
+gfc_make_links(void)
+{
+  GList *tmp1, *tmp2;
+  FileConverter *anode1, *anode2;
+  for(tmp1 = FileConverters; tmp1; tmp1 = tmp1->next)
+    {
+      for(tmp2 = FileConverters; tmp2; tmp2 = tmp2->next)
+	{
+	  if(tmp1 == tmp2)
+	    break;
+	  anode1 = tmp1->data; anode2 = tmp2->data;
+	  if(!strcmp(anode1->fromtype, anode2->totype))
+	    anode2->links = g_list_append(anode2->links,
+					  anode1);
+	}
+    }
+}
+
+/* This is a recursive function. Probably very slow,
+   who knows. The anode->tag member helps a lot tho,
+   and ensures we never get into any loops */
+static GList *
+gfc_try_path(GList *inlist,
+	     gchar *fromtype,
+	     gchar *realtotype,
+	     gint tag)
+{
+  GList *retval, *tmp, *tmp2;
+  FileConverter *anode;
+  for(tmp = inlist; tmp; tmp = tmp->next)
+    {
+      anode = tmp->data;
+      if(anode->tag == tag)
+	continue;
+      if(!strcmp(fromtype, anode->fromtype))
+	{
+	  retval = g_list_append(NULL, anode);
+	  if(!strcmp(realtotype, anode->totype))
+	      return retval;
+	  anode->tag = tag;
+	  tmp2 = gfc_try_path(anode->links, anode->totype,
+			      realtotype, tag);
+	  if(tmp2)
+	    return g_list_concat(retval, tmp2);
+	  else
+	    g_list_free(retval);
+	}
+    }
+  return NULL;
+}
+
+/* This algorithm probably isn't optimal
+   - it uses the "first-found" rather than "shortest-found"
+   path */
+static GList *
+gfc_get_path(gchar *fromtype, gchar *totype)
+{
+  static gboolean read_datfile = FALSE;
+
+  if(!read_datfile)
+    {
+      gfc_read_FileConverters();
+      gfc_make_links();
+      read_datfile = TRUE;
+    }
+
+  if(!strcmp(fromtype, totype))
+    return NULL;
+
+  return gfc_try_path(FileConverters, fromtype, totype, rand());
 }
 
 static gint
@@ -105,7 +199,7 @@ gfc_run_pipe(gchar *acmd, gint infd)
   if(fork()) /* Double-forking is good for the (zombified) soul ;-) */
     exit(0);
   else
-    execv(parts[0], parts);
+    execvp(parts[0], parts);
 
   /* ERROR IF REACHED */
   close(0); close(1);
