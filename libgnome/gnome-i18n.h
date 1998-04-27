4c6832a4095a4eb02e561efccce837b127de4287
Carsten Schaar <carsten@src.gnome.org>
1998-04-27 19:49:34 +0000

    Added gnome_i18n_get_language_list, that returns a list of languages,
    * libgnome/gnome-i18n.h: Added gnome_i18n_get_language_list, that
    returns a list of languages, depending on the users environment.

--- libgnome/gnome-i18n.h
+++ libgnome/gnome-i18n.h
@@ -33,6 +33,34 @@ BEGIN_GNOME_DECLS
 
 const char *gnome_i18n_get_language(void);
 
+/* 'gnome_i18n_get_language_list' returns a list of language strings.
+ *
+ * It searches for one of following environment variables:
+ * LANGUAGE
+ * LC_ALL
+ * 'category_name'
+ * LANG
+ *
+ * If one of these environment variables was found, it is split into
+ * pieces, whereever a ':' is found. When the environment variable included
+ * no C locale, the C locale is appended to the list of languages.
+ *
+ * Assume, you have the following environment variables set:
+ *
+ * LC_MONETARY="de_DE:es"
+ * LANG="de_DE:de:C:en"
+ * 
+ * In this case 'gnome_i18n_get_language_list ("LC_COLLATE")' returns the
+ * list: ("de_DE" "de" "C" "en").
+ *
+ * 'gnome_i18n_get_language_list ("LC_MONETARY")' returns:
+ * ("de_DE" "es" "C")
+ *
+ * The returned list must not be changed.
+ */
+
+GList      *gnome_i18n_get_language_list (const gchar *category_name);
+
 END_GNOME_DECLS
 
 #endif /* __GNOME_UTIL_H__ */
