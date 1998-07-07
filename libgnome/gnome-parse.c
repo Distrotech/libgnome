4ec2bb22cb4c1b8ff5bb1dbd62b8bc46d9713db2
Chris Toshok <toshok@src.gnome.org>
1998-07-07 05:52:34 +0000

    include stdlib.h, and (if we're on freebsd) don't include malloc.h

--- libgnome/gnome-parse.c
+++ libgnome/gnome-parse.c
@@ -16,7 +16,10 @@
    write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
    Boston, MA 02111-1307, USA.  */
 #include <config.h>
+#include <stdlib.h>
+#ifndef __FreeBSD__
 #include <malloc.h>
+#endif
 #include <glib.h>
 #include "gnome-defs.h"
 #include "gnome-parse.h"
