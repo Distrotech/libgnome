8f0354e7b3408953ad700e7fe5a21971413ca64e
James Henstridge <james@daa.com.au>
1998-10-08 11:57:18 +0000

    forgot to commit these two as well. Sorry about that.
    1998-10-08  James Henstridge  <james@daa.com.au>
    	* libgnome/libgnome.h, libgnomeui/libgnomeui.h: forgot to commit these
    	two as well.  Sorry about that.

--- libgnome/libgnome.h
+++ libgnome/libgnome.h
@@ -37,6 +37,7 @@
 #include "libgnome/gnome-triggers.h"
 #include "libgnome/gnome-util.h"
 #include "libgnome/gnome-paper.h"
+#include "libgnome/gnome-url.h"
 
 extern char *gnome_user_home_dir;
 extern char *gnome_user_dir;
