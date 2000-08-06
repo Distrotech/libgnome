06ef98ed36d37cf0c0447cf460566b20e05f4457
Martin Baulig <martin@src.gnome.org>
2000-08-06 21:12:11 +0000

    Reindented.

--- libgnome/gnome-file-selector.c
+++ libgnome/gnome-file-selector.c
@@ -1,3 +1,4 @@
+/* -*- Mode: C; c-set-style: gnu indent-tabs-mode: t; c-basic-offset: 4; tab-width: 8 -*- */
 /*
  * Copyright (C) 2000 SuSE GmbH
  * Author: Martin Baulig <baulig@suse.de>
@@ -44,8 +45,8 @@
 #include "gnome-entry.h"
 
 struct _GnomeFileSelectorPrivate {
-	GtkWidget *combo;
-	GtkWidget *entry;
+    GtkWidget *combo;
+    GtkWidget *entry;
 };
 	
 
@@ -65,195 +66,195 @@ static GnomeSelectorClass *parent_class;
 guint
 gnome_file_selector_get_type (void)
 {
-	static guint fselector_type = 0;
+    static guint fselector_type = 0;
 
-	if (!fselector_type) {
-		GtkTypeInfo fselector_info = {
-			"GnomeFileSelector",
-			sizeof (GnomeFileSelector),
-			sizeof (GnomeFileSelectorClass),
-			(GtkClassInitFunc) gnome_file_selector_class_init,
-			(GtkObjectInitFunc) gnome_file_selector_init,
-			NULL,
-			NULL,
-			NULL
-		};
+    if (!fselector_type) {
+	GtkTypeInfo fselector_info = {
+	    "GnomeFileSelector",
+	    sizeof (GnomeFileSelector),
+	    sizeof (GnomeFileSelectorClass),
+	    (GtkClassInitFunc) gnome_file_selector_class_init,
+	    (GtkObjectInitFunc) gnome_file_selector_init,
+	    NULL,
+	    NULL,
+	    NULL
+	};
 
-		fselector_type = gtk_type_unique 
-			(gnome_selector_get_type (), &fselector_info);
-	}
+	fselector_type = gtk_type_unique 
+	    (gnome_selector_get_type (), &fselector_info);
+    }
 
-	return fselector_type;
+    return fselector_type;
 }
 
 static void
 gnome_file_selector_class_init (GnomeFileSelectorClass *class)
 {
-	GnomeSelectorClass *selector_class;
-	GtkObjectClass *object_class;
-	GObjectClass *gobject_class;
+    GnomeSelectorClass *selector_class;
+    GtkObjectClass *object_class;
+    GObjectClass *gobject_class;
 
-	selector_class = (GnomeSelectorClass *) class;
-	object_class = (GtkObjectClass *) class;
-	gobject_class = (GObjectClass *) class;
+    selector_class = (GnomeSelectorClass *) class;
+    object_class = (GtkObjectClass *) class;
+    gobject_class = (GObjectClass *) class;
 
-	parent_class = gtk_type_class (gnome_selector_get_type ());
+    parent_class = gtk_type_class (gnome_selector_get_type ());
 
-	object_class->destroy = gnome_file_selector_destroy;
-	gobject_class->finalize = gnome_file_selector_finalize;
+    object_class->destroy = gnome_file_selector_destroy;
+    gobject_class->finalize = gnome_file_selector_finalize;
 
-	selector_class->get_entry_text = get_entry_text_handler;
-	selector_class->set_entry_text = set_entry_text_handler;
-	selector_class->activate_entry = activate_entry_handler;
-	selector_class->history_changed = history_changed_handler;
+    selector_class->get_entry_text = get_entry_text_handler;
+    selector_class->set_entry_text = set_entry_text_handler;
+    selector_class->activate_entry = activate_entry_handler;
+    selector_class->history_changed = history_changed_handler;
 }
 
 static gchar *
 get_entry_text_handler (GnomeSelector *selector)
 {
-	GnomeFileSelector *fselector;
-	gchar *text;
+    GnomeFileSelector *fselector;
+    gchar *text;
 
-	g_return_val_if_fail (selector != NULL, NULL);
-	g_return_val_if_fail (GNOME_IS_FILE_SELECTOR (selector), NULL);
+    g_return_val_if_fail (selector != NULL, NULL);
+    g_return_val_if_fail (GNOME_IS_FILE_SELECTOR (selector), NULL);
 
-	fselector = GNOME_FILE_SELECTOR (selector);
+    fselector = GNOME_FILE_SELECTOR (selector);
 
-	text = gtk_entry_get_text (GTK_ENTRY (fselector->_priv->entry));
-	return g_strdup (text);
+    text = gtk_entry_get_text (GTK_ENTRY (fselector->_priv->entry));
+    return g_strdup (text);
 }
 
 static void
 set_entry_text_handler (GnomeSelector *selector, const gchar *text)
 {
-	GnomeFileSelector *fselector;
+    GnomeFileSelector *fselector;
 
-	g_return_if_fail (selector != NULL);
-	g_return_if_fail (GNOME_IS_FILE_SELECTOR (selector));
+    g_return_if_fail (selector != NULL);
+    g_return_if_fail (GNOME_IS_FILE_SELECTOR (selector));
 
-	fselector = GNOME_FILE_SELECTOR (selector);
+    fselector = GNOME_FILE_SELECTOR (selector);
 
-	gtk_entry_set_text (GTK_ENTRY (fselector->_priv->entry), text);
+    gtk_entry_set_text (GTK_ENTRY (fselector->_priv->entry), text);
 }
 
 static void
 activate_entry_handler (GnomeSelector *selector)
 {
-	GnomeFileSelector *fselector;
-	gchar *text;
+    GnomeFileSelector *fselector;
+    gchar *text;
 
-	g_return_if_fail (selector != NULL);
-	g_return_if_fail (GNOME_IS_FILE_SELECTOR (selector));
+    g_return_if_fail (selector != NULL);
+    g_return_if_fail (GNOME_IS_FILE_SELECTOR (selector));
 
-	fselector = GNOME_FILE_SELECTOR (selector);
+    fselector = GNOME_FILE_SELECTOR (selector);
 
-	text = gtk_entry_get_text (GTK_ENTRY (fselector->_priv->entry));
-	gnome_selector_prepend_history (selector, TRUE, text);
+    text = gtk_entry_get_text (GTK_ENTRY (fselector->_priv->entry));
+    gnome_selector_prepend_history (selector, TRUE, text);
 
-	gtk_signal_emit_by_name (GTK_OBJECT (fselector->_priv->entry),
-				 "activate");
+    gtk_signal_emit_by_name (GTK_OBJECT (fselector->_priv->entry),
+			     "activate");
 }
 
 static void
 history_changed_handler (GnomeSelector *selector)
 {
-	GnomeFileSelector *fselector;
-	GtkWidget *list_widget;
-	GList *items = NULL;
-	GSList *c;
+    GnomeFileSelector *fselector;
+    GtkWidget *list_widget;
+    GList *items = NULL;
+    GSList *c;
 
-	g_return_if_fail (selector != NULL);
-	g_return_if_fail (GNOME_IS_FILE_SELECTOR (selector));
+    g_return_if_fail (selector != NULL);
+    g_return_if_fail (GNOME_IS_FILE_SELECTOR (selector));
 
-	fselector = GNOME_FILE_SELECTOR (selector);
+    fselector = GNOME_FILE_SELECTOR (selector);
 
-	g_message (G_STRLOC);
+    g_message (G_STRLOC);
 
-	list_widget = GTK_COMBO (fselector->_priv->combo)->list;
+    list_widget = GTK_COMBO (fselector->_priv->combo)->list;
 
-	gtk_list_clear_items (GTK_LIST (list_widget), 0, -1);
+    gtk_list_clear_items (GTK_LIST (list_widget), 0, -1);
 
-	for (c = selector->_priv->history; c; c = c->next) {
-		GnomeSelectorHistoryItem *hitem = c->data;
-		GtkWidget *item;
+    for (c = selector->_priv->history; c; c = c->next) {
+	GnomeSelectorHistoryItem *hitem = c->data;
+	GtkWidget *item;
 
-		g_print ("HISTORY: `%s'\n", hitem->text);
+	g_print ("HISTORY: `%s'\n", hitem->text);
 
-		item = gtk_list_item_new_with_label (hitem->text);
-		items = g_list_prepend (items, item);
-		gtk_widget_show_all (item);
-	}
+	item = gtk_list_item_new_with_label (hitem->text);
+	items = g_list_prepend (items, item);
+	gtk_widget_show_all (item);
+    }
 
-	items = g_list_reverse (items);
+    items = g_list_reverse (items);
 
-	gtk_list_prepend_items (GTK_LIST (list_widget), items);
+    gtk_list_prepend_items (GTK_LIST (list_widget), items);
 }
 
 static void
 entry_activated_cb (GtkWidget *widget, gpointer data)
 {
-        GnomeSelector *selector;
-	gchar *text = NULL;
+    GnomeSelector *selector;
+    gchar *text = NULL;
 
-        selector = GNOME_SELECTOR (data);
-	text = gnome_selector_get_entry_text (selector);
+    selector = GNOME_SELECTOR (data);
+    text = gnome_selector_get_entry_text (selector);
 
-	g_message (G_STRLOC ": '%s'", text);
+    g_message (G_STRLOC ": '%s'", text);
 
-	gnome_selector_freeze (selector);
+    gnome_selector_freeze (selector);
 
-	if (g_file_test (text, G_FILE_TEST_ISFILE)) {
-	    gnome_selector_append_file (selector, text);
-	} else if (g_file_test (text, G_FILE_TEST_ISDIR)) {
-	    gnome_selector_append_directory (selector, text);
-	}
+    if (g_file_test (text, G_FILE_TEST_ISFILE)) {
+	gnome_selector_append_file (selector, text);
+    } else if (g_file_test (text, G_FILE_TEST_ISDIR)) {
+	gnome_selector_append_directory (selector, text);
+    }
 
-	gnome_selector_thaw (selector);
+    gnome_selector_thaw (selector);
 
-	g_free (text);
+    g_free (text);
 }
 
 static void
 gnome_file_selector_init (GnomeFileSelector *fselector)
 {
-	fselector->_priv = g_new0 (GnomeFileSelectorPrivate, 1);
+    fselector->_priv = g_new0 (GnomeFileSelectorPrivate, 1);
 }
 
 static void
 browse_dialog_cancel (GtkWidget *widget, gpointer data)
 {
-        GnomeSelector *selector;
-	GtkFileSelection *fs;
+    GnomeSelector *selector;
+    GtkFileSelection *fs;
 
-        selector = GNOME_SELECTOR (data);
-	fs = GTK_FILE_SELECTION (selector->_priv->browse_dialog);
+    selector = GNOME_SELECTOR (data);
+    fs = GTK_FILE_SELECTION (selector->_priv->browse_dialog);
 
-	if (GTK_WIDGET (fs)->window)
-		gdk_window_lower (GTK_WIDGET (fs)->window);
-	gtk_widget_hide (GTK_WIDGET (fs));
+    if (GTK_WIDGET (fs)->window)
+	gdk_window_lower (GTK_WIDGET (fs)->window);
+    gtk_widget_hide (GTK_WIDGET (fs));
 }
 
 static void
 browse_dialog_ok (GtkWidget *widget, gpointer data)
 {
-        GnomeSelector *selector;
-	GtkFileSelection *fs;
-	gchar *filename;
+    GnomeSelector *selector;
+    GtkFileSelection *fs;
+    gchar *filename;
 
-        selector = GNOME_SELECTOR (data);
+    selector = GNOME_SELECTOR (data);
 
-	fs = GTK_FILE_SELECTION (selector->_priv->browse_dialog);
-	filename = gtk_file_selection_get_filename (fs);
+    fs = GTK_FILE_SELECTION (selector->_priv->browse_dialog);
+    filename = gtk_file_selection_get_filename (fs);
 
-	/* This is "directory safe". */
-	if (!gnome_selector_set_filename (selector, filename)) {
-		gdk_beep ();
-		return;
-	}
+    /* This is "directory safe". */
+    if (!gnome_selector_set_filename (selector, filename)) {
+	gdk_beep ();
+	return;
+    }
 
-	if (GTK_WIDGET (fs)->window)
-		gdk_window_lower (GTK_WIDGET (fs)->window);
-	gtk_widget_hide (GTK_WIDGET (fs));
+    if (GTK_WIDGET (fs)->window)
+	gdk_window_lower (GTK_WIDGET (fs)->window);
+    gtk_widget_hide (GTK_WIDGET (fs));
 }
 
 /**
@@ -275,65 +276,65 @@ gnome_file_selector_construct (GnomeFileSelector *fselector,
 			       GtkWidget *browse_dialog,
 			       guint32 flags)
 {
-	g_return_if_fail (fselector != NULL);
-	g_return_if_fail (GNOME_IS_FILE_SELECTOR (fselector));
+    g_return_if_fail (fselector != NULL);
+    g_return_if_fail (GNOME_IS_FILE_SELECTOR (fselector));
 
-	/* Create the default entry widget if requested. */
-	if (flags & GNOME_SELECTOR_DEFAULT_ENTRY_WIDGET) {
-		if (entry_widget != NULL) {
-			g_warning (G_STRLOC ": It makes no sense to use "
-				   "GNOME_SELECTOR_DEFAULT_ENTRY_WIDGET "
-				   "and pass a `entry_widget' as well.");
-			return;
-		}
-
-		entry_widget = gtk_combo_new ();
-
-		gtk_combo_disable_activate (GTK_COMBO (entry_widget));
-		gtk_combo_set_case_sensitive (GTK_COMBO (entry_widget), TRUE);
-
-		fselector->_priv->combo = entry_widget;
-		fselector->_priv->entry = GTK_COMBO (entry_widget)->entry;
-
-		gtk_signal_connect (GTK_OBJECT (fselector->_priv->entry),
-				    "activate", entry_activated_cb, fselector);
+    /* Create the default entry widget if requested. */
+    if (flags & GNOME_SELECTOR_DEFAULT_ENTRY_WIDGET) {
+	if (entry_widget != NULL) {
+	    g_warning (G_STRLOC ": It makes no sense to use "
+		       "GNOME_SELECTOR_DEFAULT_ENTRY_WIDGET "
+		       "and pass a `entry_widget' as well.");
+	    return;
 	}
 
-	/* Create the default browser dialog if requested. */
-	if (flags & GNOME_SELECTOR_DEFAULT_BROWSE_DIALOG) {
-		GtkWidget *filesel_widget;
-		GtkFileSelection *filesel;
+	entry_widget = gtk_combo_new ();
 
-		if (browse_dialog != NULL) {
-			g_warning (G_STRLOC ": It makes no sense to use "
-				   "GNOME_SELECTOR_DEFAULT_BROWSE_DIALOG "
-				   "and pass a `browse_dialog' as well.");
-			return;
-		}
+	gtk_combo_disable_activate (GTK_COMBO (entry_widget));
+	gtk_combo_set_case_sensitive (GTK_COMBO (entry_widget), TRUE);
 
-		filesel_widget = gtk_file_selection_new (dialog_title);
-		filesel = GTK_FILE_SELECTION (filesel_widget);
+	fselector->_priv->combo = entry_widget;
+	fselector->_priv->entry = GTK_COMBO (entry_widget)->entry;
 
-		gtk_signal_connect (GTK_OBJECT (filesel->cancel_button),
-				    "clicked", browse_dialog_cancel,
-				    fselector);
-		gtk_signal_connect (GTK_OBJECT (filesel->ok_button),
-				    "clicked", browse_dialog_ok,
-				    fselector);
+	gtk_signal_connect (GTK_OBJECT (fselector->_priv->entry),
+			    "activate", entry_activated_cb, fselector);
+    }
 
-		browse_dialog = GTK_WIDGET (filesel);
+    /* Create the default browser dialog if requested. */
+    if (flags & GNOME_SELECTOR_DEFAULT_BROWSE_DIALOG) {
+	GtkWidget *filesel_widget;
+	GtkFileSelection *filesel;
+
+	if (browse_dialog != NULL) {
+	    g_warning (G_STRLOC ": It makes no sense to use "
+		       "GNOME_SELECTOR_DEFAULT_BROWSE_DIALOG "
+		       "and pass a `browse_dialog' as well.");
+	    return;
 	}
 
-	gnome_selector_construct (GNOME_SELECTOR (fselector),
-				  history_id, dialog_title,
-				  entry_widget, selector_widget,
-				  browse_dialog, flags);
+	filesel_widget = gtk_file_selection_new (dialog_title);
+	filesel = GTK_FILE_SELECTION (filesel_widget);
 
-	if (flags & GNOME_SELECTOR_DEFAULT_BROWSE_DIALOG) {
-		/* We need to unref this since it isn't put in any
-		 * container so it won't get destroyed otherwise. */
-		gtk_widget_unref (browse_dialog);
-	}
+	gtk_signal_connect (GTK_OBJECT (filesel->cancel_button),
+			    "clicked", browse_dialog_cancel,
+			    fselector);
+	gtk_signal_connect (GTK_OBJECT (filesel->ok_button),
+			    "clicked", browse_dialog_ok,
+			    fselector);
+
+	browse_dialog = GTK_WIDGET (filesel);
+    }
+
+    gnome_selector_construct (GNOME_SELECTOR (fselector),
+			      history_id, dialog_title,
+			      entry_widget, selector_widget,
+			      browse_dialog, flags);
+
+    if (flags & GNOME_SELECTOR_DEFAULT_BROWSE_DIALOG) {
+	/* We need to unref this since it isn't put in any
+	 * container so it won't get destroyed otherwise. */
+	gtk_widget_unref (browse_dialog);
+    }
 }
 
 /**
@@ -350,20 +351,20 @@ GtkWidget *
 gnome_file_selector_new (const gchar *history_id,
 			 const gchar *dialog_title)
 {
-	GnomeFileSelector *fselector;
-	guint32 flags;
+    GnomeFileSelector *fselector;
+    guint32 flags;
 
-	fselector = gtk_type_new (gnome_file_selector_get_type ());
+    fselector = gtk_type_new (gnome_file_selector_get_type ());
 
-	flags = GNOME_SELECTOR_DEFAULT_ENTRY_WIDGET |
-		GNOME_SELECTOR_DEFAULT_SELECTOR_WIDGET |
-		GNOME_SELECTOR_DEFAULT_BROWSE_DIALOG |
-		GNOME_SELECTOR_WANT_BROWSE_BUTTON;
+    flags = GNOME_SELECTOR_DEFAULT_ENTRY_WIDGET |
+	GNOME_SELECTOR_DEFAULT_SELECTOR_WIDGET |
+	GNOME_SELECTOR_DEFAULT_BROWSE_DIALOG |
+	GNOME_SELECTOR_WANT_BROWSE_BUTTON;
 
-	gnome_file_selector_construct (fselector, history_id, dialog_title,
-				       NULL, NULL, NULL, flags);
+    gnome_file_selector_construct (fselector, history_id, dialog_title,
+				   NULL, NULL, NULL, flags);
 
-	return GTK_WIDGET (fselector);
+    return GTK_WIDGET (fselector);
 }
 
 GtkWidget *
@@ -374,49 +375,49 @@ gnome_file_selector_new_custom (const gchar *history_id,
 				GtkWidget *browse_dialog,
 				guint32 flags)
 {
-	GnomeFileSelector *fselector;
+    GnomeFileSelector *fselector;
 
-	fselector = gtk_type_new (gnome_file_selector_get_type ());
+    fselector = gtk_type_new (gnome_file_selector_get_type ());
 
-	gnome_file_selector_construct (fselector, history_id,
-				       dialog_title, entry_widget,
-				       selector_widget, browse_dialog,
-				       flags);
+    gnome_file_selector_construct (fselector, history_id,
+				   dialog_title, entry_widget,
+				   selector_widget, browse_dialog,
+				   flags);
 
-	return GTK_WIDGET (fselector);
+    return GTK_WIDGET (fselector);
 }
 
 
 static void
 gnome_file_selector_destroy (GtkObject *object)
 {
-	GnomeFileSelector *fselector;
+    GnomeFileSelector *fselector;
 
-	/* remember, destroy can be run multiple times! */
+    /* remember, destroy can be run multiple times! */
 
-	g_return_if_fail (object != NULL);
-	g_return_if_fail (GNOME_IS_SELECTOR (object));
+    g_return_if_fail (object != NULL);
+    g_return_if_fail (GNOME_IS_SELECTOR (object));
 
-	fselector = GNOME_FILE_SELECTOR (object);
+    fselector = GNOME_FILE_SELECTOR (object);
 
-	if (GTK_OBJECT_CLASS (parent_class)->destroy)
-		(* GTK_OBJECT_CLASS (parent_class)->destroy) (object);
+    if (GTK_OBJECT_CLASS (parent_class)->destroy)
+	(* GTK_OBJECT_CLASS (parent_class)->destroy) (object);
 }
 
 static void
 gnome_file_selector_finalize (GObject *object)
 {
-	GnomeFileSelector *fselector;
+    GnomeFileSelector *fselector;
 
-	g_return_if_fail (object != NULL);
-	g_return_if_fail (GNOME_IS_SELECTOR (object));
+    g_return_if_fail (object != NULL);
+    g_return_if_fail (GNOME_IS_SELECTOR (object));
 
-	fselector = GNOME_FILE_SELECTOR (object);
+    fselector = GNOME_FILE_SELECTOR (object);
 
-	g_free (fselector->_priv);
-	fselector->_priv = NULL;
+    g_free (fselector->_priv);
+    fselector->_priv = NULL;
 
-	if (G_OBJECT_CLASS (parent_class)->finalize)
-		(* G_OBJECT_CLASS (parent_class)->finalize) (object);
+    if (G_OBJECT_CLASS (parent_class)->finalize)
+	(* G_OBJECT_CLASS (parent_class)->finalize) (object);
 }
 
--- libgnome/gnome-file-selector.h
+++ libgnome/gnome-file-selector.h
@@ -1,3 +1,4 @@
+/* -*- Mode: C; c-set-style: gnu indent-tabs-mode: t; c-basic-offset: 4; tab-width: 8 -*- */
 /*
  * Copyright (C) 2000 SuSE GmbH
  * Author: Martin Baulig <baulig@suse.de>
@@ -53,14 +54,14 @@ typedef struct _GnomeFileSelectorPrivate  GnomeFileSelectorPrivate;
 typedef struct _GnomeFileSelectorClass    GnomeFileSelectorClass;
 
 struct _GnomeFileSelector {
-        GnomeSelector selector;
+    GnomeSelector selector;
         
-        /*< private >*/
-        GnomeFileSelectorPrivate *_priv;
+    /*< private >*/
+    GnomeFileSelectorPrivate *_priv;
 };
 
 struct _GnomeFileSelectorClass {
-        GnomeSelectorClass parent_class;
+    GnomeSelectorClass parent_class;
 };
 
 
