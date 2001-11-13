9c88a58f6a1565d7998cbd3d7004fb6b1e020fcd
Michael Meeks <michael@ximian.com>
2001-11-13 23:27:17 +0000

    update for new bonobo.
    2001-11-14  Michael Meeks  <michael@ximian.com>
    
    	* gnome-macros.h: update for new bonobo.

--- libgnome/gnome-macros.h
+++ libgnome/gnome-macros.h
@@ -25,6 +25,8 @@
   @NOTATION@
 */
 
+#include <bonobo/bonobo-macros.h>
+
 #ifndef GNOME_MACROS_H
 #define GNOME_MACROS_H
 
@@ -34,61 +36,25 @@
  * the get_type method and the parent_class static variable. */
 #define GNOME_CLASS_BOILERPLATE(type, type_as_function,			\
 				parent_type, parent_type_macro)		\
-	GNOME_BOILERPLATE(type, type_as_function, type,			\
+	BONOBO_BOILERPLATE(type, type_as_function, type,		\
 			  parent_type, parent_type_macro,		\
 			  GNOME_REGISTER_TYPE)
 #define GNOME_REGISTER_TYPE(type, type_as_function, corba_type,		\
 			    parent_type, parent_type_macro)		\
 	g_type_register_static (parent_type_macro, #type, &object_info, 0)
-#define GNOME_BOILERPLATE(type, type_as_function, corba_type,		\
-			  parent_type, parent_type_macro,		\
-			  register_type_macro)				\
-static void type_as_function ## _class_init    (type ## Class *klass);	\
-static void type_as_function ## _instance_init (type          *object);	\
-static parent_type ## Class *parent_class = NULL;			\
-static void								\
-type_as_function ## _class_init_trampoline (gpointer klass,		\
-					    gpointer data)		\
-{									\
-	parent_class = g_type_class_ref (parent_type_macro);		\
-	type_as_function ## _class_init (klass);			\
-}									\
-GType									\
-type_as_function ## _get_type (void)					\
-{									\
-	static GType object_type = 0;					\
-	if (object_type == 0) {						\
-		static const GTypeInfo object_info = {			\
-		    sizeof (type ## Class),				\
-		    NULL,		/* base_init */			\
-		    NULL,		/* base_finalize */		\
-		    type_as_function ## _class_init_trampoline,		\
-		    NULL,		/* class_finalize */		\
-		    NULL,               /* class_data */		\
-		    sizeof (type),					\
-		    0,                  /* n_preallocs */		\
-		    (GInstanceInitFunc) type_as_function ## _instance_init \
-		};							\
-		object_type = register_type_macro			\
-			(type, type_as_function, corba_type,		\
-			 parent_type, parent_type_macro);		\
-	}								\
-	return object_type;						\
-}
 
 /* Just call the parent handler.  This assumes that there is a variable
  * named parent_class that points to the (duh!) parent class.  Note that
  * this macro is not to be used with things that return something, use
  * the _WITH_DEFAULT version for that */
 #define GNOME_CALL_PARENT(parent_class_cast, name, args)		\
-	((parent_class_cast(parent_class)->name != NULL) ?		\
-	 parent_class_cast(parent_class)->name args : (void)0)
+	BONOBO_CALL_PARENT (parent_class_cast, name, args)
 
 /* Same as above, but in case there is no implementation, it evaluates
  * to def_return */
 #define GNOME_CALL_PARENT_WITH_DEFAULT(parent_class_cast,		\
-					       name, args, def_return)	\
-	((parent_class_cast(parent_class)->name != NULL) ?		\
-	 parent_class_cast(parent_class)->name args : def_return)
+				       name, args, def_return)		\
+	BONOBO_CALL_PARENT_WITH_DEFAULT (				\
+		parent_class_cast, name, args, def_return)
 
 #endif /* GNOME_MACROS_H */
