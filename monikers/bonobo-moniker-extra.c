472fa3302f8ea27504e4cf731294c0926c3c2980
Michael Meeks <michael@ximian.com>
2002-01-09 16:35:27 +0000

    add monikers.
    2002-01-09  Michael Meeks  <michael@ximian.com>
    
    	* Makefile.am: add monikers.

--- monikers/bonobo-moniker-extra.c
+++ monikers/bonobo-moniker-extra.c
@@ -11,20 +11,20 @@ bonobo_extra_moniker_factory (BonoboGenericFactory *this,
 {
         g_return_val_if_fail (object_id != NULL, NULL);
 
-	if (!strcmp (object_id, "OAFIID:Bonobo_Moniker_Config")) {
+	if (!strcmp (object_id, "OAFIID:GNOME_Moniker_Config")) {
 		return BONOBO_OBJECT (bonobo_moniker_simple_new (
 			"config:", bonobo_moniker_config_resolve));
-	}
-	else if (!strcmp (object_id, "OAFIID:Bonobo_Moniker_ConfIndirect")) {
+
+	} else if (!strcmp (object_id, "OAFIID:GNOME_Moniker_ConfIndirect")) {
                 return BONOBO_OBJECT (bonobo_moniker_simple_new (
                         "conf_indirect:", bonobo_moniker_conf_indirect_resolve));
-	}
-	else
+
+	} else
                 g_warning ("Failing to manufacture a '%s'", object_id);
 
 	return NULL;
 }
 
-BONOBO_ACTIVATION_SHLIB_FACTORY ("OAFIID:Bonobo_Moniker_extra_Factory",
+BONOBO_ACTIVATION_SHLIB_FACTORY ("OAFIID:GNOME_Moniker_std_Factory",
                                  "Extra bonobo moniker",
                                  bonobo_extra_moniker_factory, NULL);
