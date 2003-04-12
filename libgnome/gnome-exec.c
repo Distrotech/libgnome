c61c8388999a8134850ead1630367f2a4445525a
Arvind Samptur <arvind.samptur@wipro.com>
2003-04-12 06:17:54 +0000

    waitpid() could fail due to a interrupted system call, loop till you
    2003-04-12  Arvind Samptur <arvind.samptur@wipro.com>
            * libgnome/gnome-exec.c: (gnome_execute_async_with_env_fds):
            waitpid() could fail due to a interrupted system call, loop till you
            succeed. Bug #110100
    
            Reviewed by: Mark McLoughlin <mark@skynet.ie>

--- libgnome/gnome-exec.c
+++ libgnome/gnome-exec.c
@@ -211,7 +211,7 @@ gnome_execute_async_with_env_fds (const char *dir, int argc,
 
   /* do this after the read's in case some OS's handle blocking on pipe writes
      differently */
-  waitpid(immediate_child_pid, &itmp, 0); /* eat zombies */
+   while ((waitpid(immediate_child_pid, &itmp, 0)== -1) && (errno == EINTR)); /* eat zombies */
 
   close(parent_comm_pipes[0]);
 
