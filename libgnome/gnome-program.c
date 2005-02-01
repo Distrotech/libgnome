c6e1e84eaf85a721be2ed0edadd640ad89baee54
Kjartan Maraas <kmaraas@gnome.org>
2005-02-01 13:59:59 +0000

    Merge fix for 165564.
    2005-02-01  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* libgnome/gnome-program.c: Merge fix for 165564.

--- libgnome/gnome-program.c
+++ libgnome/gnome-program.c
@@ -1928,6 +1928,8 @@ gnome_program_init_paramv (GType type,
                            int argc, char **argv,
                            guint nparams, GParameter *params)
 {
+    va_list args;
+
     return gnome_program_init_common (type, app_id, app_version, module_info,
-				      argc, argv, NULL, 0, nparams, params);
+				      argc, argv, NULL, args, nparams, params);
 }
