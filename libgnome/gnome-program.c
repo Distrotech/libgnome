40474dc5d18639b6f87346eceb94a50692bc20db
Kjartan Maraas <kmaraas@gnome.org>
2005-02-14 16:43:51 +0000

    init args to 0 to avoid complaints from gcc. Fix for my previous commit.
    2005-02-14  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* libgnome/gnome-program.c: (gnome_program_init_paramv):
    	init args to 0 to avoid complaints from gcc. Fix for my previous
    	commit.

--- libgnome/gnome-program.c
+++ libgnome/gnome-program.c
@@ -1928,8 +1928,9 @@ gnome_program_init_paramv (GType type,
                            int argc, char **argv,
                            guint nparams, GParameter *params)
 {
-    va_list args;
+    va_list args = 0;
 
     return gnome_program_init_common (type, app_id, app_version, module_info,
 				      argc, argv, NULL, args, nparams, params);
+
 }
