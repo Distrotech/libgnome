9a5f4f8eddbbdb11c79ff81b064530240528d348
Tor Lillqvist <tml@novell.com>
2005-08-20 22:29:44 +0000

    Once more fix the system codepage prefix setup logic.
    2005-08-21  Tor Lillqvist  <tml@novell.com>
    
    	* libgnome/dllmain.c (gnome_win32_get_prefixes): Once more fix the
    	system codepage prefix setup logic.

--- libgnome/dllmain.c
+++ libgnome/dllmain.c
@@ -102,6 +102,9 @@ gnome_win32_get_prefixes (gpointer  hmodule,
                           char    **full_prefix,
                           char    **cp_prefix)
 {
+        wchar_t wcbfr[1000];
+        char cpbfr[1000];
+
         g_return_if_fail (full_prefix != NULL);
         g_return_if_fail (cp_prefix != NULL);
 
@@ -110,21 +113,24 @@ gnome_win32_get_prefixes (gpointer  hmodule,
 
         if (G_WIN32_HAVE_WIDECHAR_API ()) {
                 /* NT-based Windows has wide char API */
-                wchar_t wcbfr[1000];
                 if (GetModuleFileNameW ((HMODULE) hmodule, wcbfr, G_N_ELEMENTS (wcbfr))) {
                         *full_prefix = g_utf16_to_utf8 (wcbfr, -1,
                                                         NULL, NULL, NULL);
-                        if (GetShortPathNameW (wcbfr, wcbfr,
-                                               G_N_ELEMENTS (wcbfr)))
-                                *cp_prefix = g_utf16_to_utf8 (wcbfr, -1,
-                                                              NULL, NULL, NULL);
+                        if (GetShortPathNameW (wcbfr, wcbfr, G_N_ELEMENTS (wcbfr)) &&
+                            /* Short pathnames always contain only
+                             * ASCII, I think, but just in case, be
+                             * prepared.
+                             */
+                            WideCharToMultiByte (CP_ACP, 0, wcbfr, -1,
+                                                 cpbfr, G_N_ELEMENTS (cpbfr),
+                                                 NULL, NULL))
+                                *cp_prefix = g_strdup (cpbfr);
                         else if (*full_prefix)
                                 *cp_prefix = g_locale_from_utf8 (*full_prefix, -1,
                                                                  NULL, NULL, NULL);
                 }
         } else {
                 /* Win9x */
-                char cpbfr[1000];
                 if (GetModuleFileNameA ((HMODULE) hmodule, cpbfr, G_N_ELEMENTS (cpbfr))) {
                         *full_prefix = g_locale_to_utf8 (cpbfr, -1,
                                                          NULL, NULL, NULL);
@@ -142,24 +148,15 @@ gnome_win32_get_prefixes (gpointer  hmodule,
                         *p = '\0';
         }
 
+        /* cp_prefix is in system codepage */
         if (*cp_prefix != NULL) {
-                gchar *p = strrchr (*cp_prefix, '\\');
+                gchar *p = _mbsrchr (*cp_prefix, '\\');
                 if (p != NULL)
                         *p = '\0';
       
-                p = strrchr (*cp_prefix, '\\');
+                p = _mbsrchr (*cp_prefix, '\\');
                 if (p && (g_ascii_strcasecmp (p + 1, "bin") == 0))
                         *p = '\0';
-
-                /* In case we couldn't get the short file name, or the
-                 * short file name isn't in the system codepage after
-                 * all, conversion to locale charset (system codepage)
-                 * might fail. The called should be ready for a
-                 * returned NULL.
-                 */
-                p = g_locale_from_utf8 (*cp_prefix, -1, NULL, NULL, NULL);
-                g_free (*cp_prefix);
-                *cp_prefix = p;
         }
 }
 
